771ed84b5110fd3482875b98ad5988b5335db38e
U-hyeon <westwoodyousoro@gmail.com>
2023-07-25 19:00:12 +0900

    230725 Java practice

---
+++ MyProfile/src/MyProfile.java
@@ -0,0 +1,7 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.print("My name");
+        System.out.print("Born in 1996");
+        System.out.print("175cm / 55kg");
+    }
+}
