3eb0038b560b700d49817a38bf2d4821f04084da
U-hyeon <westwoodyousoro@gmail.com>
2023-07-28 18:33:48 +0900

    230728 Java practice

---
+++ VendingMachine/src/Parent.java
@@ -0,0 +1,9 @@
+public class Parent {
+    public int i = 5;
+    public void printI() {
+        System.out.println("parent - printI() : " + i);
+    }
+    public void printII() {
+        System.out.println("parent - printII() : " + i*2);
+    }
+}
