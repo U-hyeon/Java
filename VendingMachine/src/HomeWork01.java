47016b455966fb5e5c29729ca57b411924c3f712
U-hyeon <westwoodyousoro@gmail.com>
2023-08-07 15:48:13 +0900

    230807 Java practice

---
+++ VendingMachine/src/HomeWork01.java
@@ -0,0 +1,15 @@
+import java.io.InputStream;
+
+/*
+키보드로부터 한줄씩 입력받아 화면에 출력하시오
+
+1. txt 파일로부터 한줄씩 입력받아 화면에 출력한다.
+2. 키보드로부터 한줄씩 입력받아 파일에 출력한다. (파일명은 아규먼트로 받아들인다.
+3. txt 파일로부터 한줄씩 입력받아 다른 파일에 출력한다.
+ */
+
+public class HomeWork01 {
+    public static void main(String[] args){
+
+    }
+}
