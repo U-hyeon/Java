e86501de43da9517a5aa72e674dd3bf9dfa7f1d0
U-hyeon <wisebuddyu@gmail.com>
2024-08-21 23:59:43 +0900

    240821 java practice bean by active

---
+++ demo/src/main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 수동으로 빈을 등록하는 파일
+ * MemberService, MemoryMemberRepository 파일의 @Autowired로 대체가능
+ */
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -6,9 +6,22 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+/*
+    // 필드주입
+    @Autowired private final MemberService memberService;
+*/
+/*
+    // setter를 통한 주입. setter injection
+    private MemberService memberService; // final이 아님
 
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+ */
     private final MemberService memberService;
 
+    // 생성자주입 (권장)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
